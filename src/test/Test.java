83d9593a5d2f6e5cd84065d4d181f7f54f83d3cb
Heejay Kong <heejaykong@naver.com>
2018-12-21 23:54:15 +0900

    modify test

--- src/test/Test.java
+++ src/test/Test.java
@@ -7,32 +7,32 @@ class Test {
 
 //AppointmentTest
    @Test
-   void testSqlUpdate() {
+   public void testSqlUpdate() throws Exception{
       assertTrue(Appointments_Management.sqlUpdate("INSERT INTO appoint_tbl(date,people,place) VALUES('상대방','0109283722','1288428392')");
    }
    @Test
-   void testConnprocess() {
+   public void testConnprocess() throws Exception{
       assertTrue(Appointments_Management.connprocess());
    }
 
 //ContactsTest
    @Test
-   void testSqlUpdate() {
+   public void testSqlUpdate() throws Exception{
       assertTrue(Contact_Management.sqlUpdate("INSERT INTO appoint_tbl(date,people,place) VALUES('상대방','0109283722','1288428392')");
    }
    @Test
-   void testConnprocess() {
+   public void testConnprocess() throws Exception{
       assertTrue(Contact_Management.connprocess());
    }
 
 //ToDoTest
    @Test
-   void testSqlUpdate() {
+   public void testSqlUpdate() throws Exception{
       assertTrue(ToDo_Management.sqlUpdate("INSERT INTO appoint_tbl(date,people,place) VALUES('상대방','0109283722','1288428392')");
    }
    
    @Test
-   void testConnprocess() {
+   public void testConnprocess() throws Exception{
       assertTrue(ToDo_Management.connprocess());
    }
 }
